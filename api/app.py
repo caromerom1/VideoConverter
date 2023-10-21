0ede0122eb217728dfa5ab6e5ec197f0625822f4
Camilo Romero <ca.romerom1@uniandes.edu.co>
2023-10-21 16:04:03 -0500

    feat: Include signup & login endpoints
    * Fix docker API_PORT
    * Include .env example
    * Update gitignore

--- api/app.py
+++ api/app.py
@@ -1,1 +1,18 @@
-print('api.py')
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/api/auth/signup", methods=["POST"])
+def signup():
+    return "Hello, World!"
+
+@app.route("/api/auth/login", methods=["POST"])
+def login():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    API_PORT = os.environ.get("API_PORT", 5000)
+    app.run(debug=True, port=API_PORT, host="0.0.0.0")
